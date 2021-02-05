748842eb4c7669991869640fd12caaec94c6328f
Robert <silent.jaguar@gmail.com>
2021-02-05 14:32:17 +0300

    Downloaded Lesson1

---
+++ Lesson 1/Lesson1Ex7/src/Les1Ex7/Ex7.java
@@ -0,0 +1,16 @@
+package Les1Ex7;
+
+public class Ex7 {
+
+    public static void main(String[] args){
+
+        helloName("Аня");
+        helloName("Катя");
+        helloName("Инна");
+    }
+
+    public static void helloName (String n){
+        System.out.println("Привет," + n);
+    }
+
+}
---
+++ Lesson 1/Lesson1_Ex1/src/Less1Ex1/L1Ex1.java
@@ -0,0 +1,8 @@
+package Less1Ex1;
+
+public class L1Ex1 {
+
+    public static void main (String[] args){
+
+    }
+}
