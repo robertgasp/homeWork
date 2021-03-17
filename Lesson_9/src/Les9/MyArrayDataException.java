30447f87b2bb96e6ee5cbf5a2c98ae8c080566bb
robert <robert@localhost.localdomain>
2021-03-17 09:44:15 +0300

    modified Lesson 9

--- Lesson_9/src/Les9/MyArrayDataException.java
+++ Lesson_9/src/Les9/MyArrayDataException.java
@@ -9,10 +9,7 @@ public class MyArrayDataException extends NumberFormatException {
 
     }
 
-    public MyArrayDataException(String s) {
-        super(s);
 
-    }
 
 
     public MyArrayDataException(int i, int j) {
