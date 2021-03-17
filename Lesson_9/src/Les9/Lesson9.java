1d51ad92fbe17dc75b291dacc9d4efb2541146e1
robert <robert@localhost.localdomain>
2021-03-17 21:45:02 +0300

    Modified Lesson 9

--- Lesson_9/src/Les9/Lesson9.java
+++ Lesson_9/src/Les9/Lesson9.java
@@ -1,17 +1,15 @@
 package Les9;
 
+
 public class Lesson9 {
 
     static int sizeX = 4;
-    static int sizeY = 5;
+    static int sizeY = 4;
 
     public static void main(String[] args) {
 
 
         String[][] arr = new String[sizeX][sizeY];
-
-        checkArraySize(arr);
-
 
         int k = 0; //Для заполнения массива цифрами в формате String
         for (int i = 0; i < sizeX; i++) {
@@ -23,22 +21,22 @@ public class Lesson9 {
 
         arr[1][1] = "ф";   //это специально для проверки MyArrayDataException
 
-        checkArrayData(arr);
-    }
 
-
-    private static void checkArraySize(String[][] arr) {
         try {
-            if (arr.length != 4 || arr[0].length != 4) {
-                throw new MyArraySizeException();
-            }
+            checkArrayData(arr);
         } catch (MyArraySizeException e) {
             e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
         }
+
     }
 
+    private static void checkArrayData(String[][] arr) throws MyArraySizeException, MyArrayDataException {
 
-    private static void checkArrayData(String[][] arr) {
+        if (arr.length != 4 || arr[0].length != 4) {
+            throw new MyArraySizeException();
+        }
 
         int sum = 0;
 
@@ -46,13 +44,12 @@ public class Lesson9 {
             for (int j = 0; j < arr.length; j++) {
 
                 try {
+                    Integer.parseInt(arr[i][j]);
                     sum += Integer.parseInt(arr[i][j]);
                 } catch (NumberFormatException e) {
                     throw new MyArrayDataException(i, j);
                 }
             }
         }
-
     }
-
 }
--- Lesson_9/src/Les9/MyArrayDataException.java
+++ Lesson_9/src/Les9/MyArrayDataException.java
@@ -9,13 +9,9 @@ public class MyArrayDataException extends NumberFormatException {
 
     }
 
-
-
-
     public MyArrayDataException(int i, int j) {
         super("Неверный тип данных в ячейке [" + i + "][" + j + "]");
-        printStackTrace();
-
     }
+
 
 }
